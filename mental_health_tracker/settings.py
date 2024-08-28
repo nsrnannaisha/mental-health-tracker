eec7f26fc80d201abc5ddbab8910b26355013398
nsrnannaisha <nisrinaannaisha@gmail.com>
2024-08-28 22:06:24 +0700

    fixed local host

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_8@6pmjeab&eew@0h%fm!i%)#&%2gi)!dz*=s&cj&v8eulc1(f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "nisrina-annaisha-mentalhealthtrackerr.pbp.cs.ui.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "nisrina-annaisha-mentalhealthtrackerr.pbp.cs.ui.ac.id"]
 
 
 # Application definition
