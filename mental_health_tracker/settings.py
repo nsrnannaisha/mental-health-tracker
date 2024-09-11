7d466394c4da3d441ba14b16d9c65db518e5c988
nsrnannaisha <nisrinaannaisha@gmail.com>
2024-09-11 16:15:21 +0700

    Form dan Data Delivery update

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -55,8 +55,9 @@ ROOT_URLCONF = 'mental_health_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], 
         'APP_DIRS': True,
+
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -122,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://nisrina.annaisha:4XqHnfSKUn62sRqrp7PBq2xxQQJMSMHY@pbp.cs.ui.ac.id/nisrina.annaisha/mentalhealthtrackerr", "https://nisrina.annaisha:4XqHnfSKUn62sRqrp7PBq2xxQQJMSMHY@pbp.cs.ui.ac.id/nisrina.annaisha/mentalhealthtrackerr"]
+
