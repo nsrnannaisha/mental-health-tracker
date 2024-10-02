fb916d0b93c2528c30533df090839805e42a648d
nsrnannaisha <nisrinaannaisha@gmail.com>
2024-10-02 16:14:20 +0700

    deploy trial

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_8@6pmjeab&eew@0h%fm!i%)#&%2gi)!dz*=s&cj&v8eulc1(f
 
 # SECURITY WARNING: don't run with debug turned on in production!
 PRODUCTION = os.getenv("PRODUCTION", False)
-DEBUG = True
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "nisrina-annaisha-mentalhealthtrackerr.pbp.cs.ui.ac.id"]
 
